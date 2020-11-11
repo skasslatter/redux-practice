8b469fda08897b6e42550d20fa6d481e2aac53df
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-11-11 15:29:17 +0100

    ✨ Added stade modifier switch case

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,5 @@
 import { ITodo } from './models/todo';
+import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO, REMOVE_ALL_TODOS } from './actions';
 
 export interface IAppState {
   todos: ITodo[];
@@ -7,9 +8,49 @@ export interface IAppState {
 
 export const INITIAL_STATE: IAppState = {
   todos: [],
-  lastUpdate: null,
+  lastUpdate: null
 };
 
-export function rootReducer(state, action) {
+export function rootReducer(state, action): any {
+  switch (action.type) {
+    case ADD_TODO:
+      action.todo.id = state.todos.length + 1;
+      return Object.assign({}, state, {
+        todos: state.todos.concat(Object.assign({}, action.todo)),
+        lastUpdate: new Date()
+      });
+
+    case TOGGLE_TODO:
+      const todo: ITodo = state.todos.find((t) => {
+        return t.id === action.id;
+      });
+      const todoIndex = state.todos.indexOf(todo);
+
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos.slice(0, todoIndex),
+          Object.assign({}, todo, { isCompleted: !todo.isCompleted }),
+          ...state.todos.slice(todoIndex + 1)
+        ],
+        lastUpdate: new Date()
+      });
+
+
+    case REMOVE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.filter((t) => {
+          return t.id !== action.id;
+        }),
+        lastUpdate: new Date()
+      });
+
+    case REMOVE_ALL_TODOS:
+      return Object.assign({}, state, {
+        todos: [],
+        lastUpdate: new Date()
+      });
+
+  }
   return state;
+
 }
