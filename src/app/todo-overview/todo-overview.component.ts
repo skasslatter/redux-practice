591a5dfdfc1f69ddd7ab02c059442b2f1f82bf78
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-11-11 15:49:10 +0100

    🎨 Added logic fir todo-overview component

--- src/app/todo-overview/todo-overview.component.ts
+++ src/app/todo-overview/todo-overview.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgRedux, select } from '@angular-redux/store';
+import { IAppState } from '../store';
+import { REMOVE_ALL_TODOS } from '../actions';
+
 
 @Component({
   selector: 'app-todo-overview',
   templateUrl: './todo-overview.component.html',
   styleUrls: ['./todo-overview.component.scss']
 })
-export class TodoOverviewComponent implements OnInit {
+export class TodoOverviewComponent {
+  @select() todos;
+  // another possibility e.g.: @select('todos) todosArray;
+  @select() lastUpdate;
 
-  constructor() { }
+  constructor(
+    private ngRedux: NgRedux<IAppState>
+  ) { }
 
-  ngOnInit(): void {
+  clearTodos(): void {
+    this.ngRedux.dispatch({ type: REMOVE_ALL_TODOS });
   }
-
 }
+
