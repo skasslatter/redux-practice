45aff87e4e644215bb2e2ddc4b422c76a9a568a4
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-11-12 15:07:37 +0100

    🎨 refactored store

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,51 +4,84 @@ import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO, REMOVE_ALL_TODOS } from './actions'
 export interface IAppState {
   todos: ITodo[];
   lastUpdate: Date;
+  nextId: number;
 }
 
 export const INITIAL_STATE: IAppState = {
   todos: [],
-  lastUpdate: null
+  lastUpdate: null,
+  nextId: 1,
 };
 
 export function rootReducer(state, action): any {
   switch (action.type) {
     case ADD_TODO:
-      action.todo.id = state.todos.length + 1;
-      return Object.assign({}, state, {
-        todos: state.todos.concat(Object.assign({}, action.todo)),
-        lastUpdate: new Date()
-      });
+      action.todo.id = state.nextId;
+
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            ...action.todo
+          }
+        ],
+        lastUpdate: new Date(),
+        nextId: state.nextId + 1,
+      };
+    // return Object.assign({}, state, {
+    //   todos: state.todos.concat(Object.assign({}, action.todo)),
+    //   lastUpdate: new Date()
+    // });
 
     case TOGGLE_TODO:
       const todo: ITodo = state.todos.find((t) => {
         return t.id === action.id;
       });
       const todoIndex = state.todos.indexOf(todo);
-
-      return Object.assign({}, state, {
+      return {
+        ...state,
         todos: [
           ...state.todos.slice(0, todoIndex),
-          Object.assign({}, todo, { isCompleted: !todo.isCompleted }),
+          {
+            ...todo,
+            isCompleted: !todo.isCompleted
+          },
           ...state.todos.slice(todoIndex + 1)
         ],
         lastUpdate: new Date()
-      });
-
+      };
+    // return Object.assign({}, state, {
+    //   todos: [
+    //     ...state.todos.slice(0, todoIndex),
+    //     {
+    //       ...todo,
+    //       isCompleted: !todo.isCompleted
+    //     },
+    //     ...state.todos.slice(todoIndex + 1)
+    //   ],
+    //   lastUpdate: new Date()
+    // });
 
     case REMOVE_TODO:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         todos: state.todos.filter((t) => {
           return t.id !== action.id;
         }),
         lastUpdate: new Date()
-      });
+      };
 
     case REMOVE_ALL_TODOS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         todos: [],
         lastUpdate: new Date()
-      });
+      };
+    // return Object.assign({}, state, {
+    //   todos: [],
+    //   lastUpdate: new Date()
+    // });
 
   }
   return state;
