d033b815ad6e34b872b0cb86a1ff48a22837f6e8
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-11-12 14:42:46 +0100

    ✨ added todo-list logic and template

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgRedux, select } from '@angular-redux/store';
+import { IAppState } from '../../store';
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from '../../actions';
+import { ITodo } from '../../models/todo';
+
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent {
+  @select() todos;
 
-  constructor() { }
+  model: ITodo = {
+    id: 0,
+    description: '',
+    responsible: '',
+    priority: 'low',
+    isCompleted: false
+  };
 
-  ngOnInit(): void {
+  constructor(
+    private ngRedux: NgRedux<IAppState>
+  ) { }
+
+  onSubmit(): void {
+    this.ngRedux.dispatch({ type: ADD_TODO, todo: this.model });
   }
 
+  toggleTodo(todo): void {
+    this.ngRedux.dispatch({ type: TOGGLE_TODO, id: todo.id });
+  }
+
+  removeTodo(todo): void {
+    this.ngRedux.dispatch({ type: REMOVE_TODO, id: todo.id });
+  }
 }
